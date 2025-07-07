e619192ad1b58c2963b9f113abe3baa16c50de27
racket207-hub <rocco.lore@branchina.edu.it>
2025-07-07 12:36:34 +0200

    Commento di LibraryItem.java

--- LibraryItem.java
+++ LibraryItem.java
@@ -1,16 +1,36 @@
+/**
+ * @author Salvatore Garufi
+ * @since 07/07/2025
+ * si inizializza la classe LibraryItem
+ * e le sue variabili private book e quantity
+ */
 public class LibraryItem {
     private IBook book;
     private int quantity;
 
+    /**
+     * si inizializza il metodo con i seguenti parametri
+     * @param book
+     * @param quantity
+     * e successivamente i usa il "this" perchè il nome usato è uguale al nome della variabile
+     */
     public LibraryItem(IBook book, int quantity) {
         this.book = book;
         this.quantity = quantity;
     }
 
+    /**
+     * tramita getBook si ritorna:
+     * @return book
+     */
     public IBook getBook() {
         return book;
     }
 
+    /**
+     * tramite getQuantity si ritorna:
+     * @return quantity
+     */
     public int getQuantity() {
         return quantity;
     }
