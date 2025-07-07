13dce7d439b836d767722b5747460acc59a4e675
racket207-hub <rocco.lore@branchina.edu.it>
2025-07-07 12:46:03 +0200

    Commento di Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
+/**
+ * @author Salvatore Garufi
+ * @since 07/07/2025
+ * si crea un array di 10 elementi col nome di Library
+ */
 public class Main {
     public static void main(String[] args) {
         Library library = new Library(10);
 
+        /**
+         * si assegna il nome alle due variabili sul libro stampato e sul libro digitale
+         */
         PrintedBook pb = new PrintedBook("1984", "George Orwell", 328);
         EBook eb = new EBook("Digital Fortress", "Dan Brown", 356, 1.5);
 
