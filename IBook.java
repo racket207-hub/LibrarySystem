61a9dbc5954ca95ed7ba3861625b7884d52aae09
racket207-hub <rocco.lore@branchina.edu.it>
2025-07-07 12:18:38 +0200

    Commento di IBook

--- IBook.java
+++ IBook.java
@@ -1,3 +1,11 @@
+/**
+ * @author Salvatore Garufi
+ * @since 07/07/2025
+ *
+ * Inizializzazione della Interfaccia IBook
+ * con i suoi metodi stringa  getTitle(), getAuthor()
+ * e la sua variabile int getPages()
+ */
 public interface IBook {
     String getTitle();
     String getAuthor();
