da13268b69670c24d87cbf0cd748cf0168d4401d
racket207-hub <rocco.lore@branchina.edu.it>
2025-07-07 12:15:17 +0200

    Commento del codice

--- EBook.java
+++ EBook.java
@@ -1,15 +1,23 @@
 /**
  * @author Salvatore Garufi
  * @since 07/07/2025
- * 
- *
+ */
+
+/**
+ * si inizializza la classe Ebook che implementa la classe IBook
  */
 public class EBook implements IBook {
+    /**
+     *  si inizializzano le seguenti variabili
+     */
     private String title;
     private String author;
     private int pages;
     private double fileSizeMB;
 
+/**
+* Si usa la classe Ebook con i parametri che abbiamo inizializzato precedentemente
+ */
     public EBook(String title, String author, int pages, double fileSizeMB) {
         this.title = title;
         this.author = author;
