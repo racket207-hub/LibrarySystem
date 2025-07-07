59f70ccc36b67ad2ad428b7137e62e644df7717e
racket207-hub <rocco.lore@branchina.edu.it>
2025-07-07 12:49:39 +0200

    Commento di Library.java

--- Library.java
+++ Library.java
@@ -1,7 +1,7 @@
 /**
  * @author Salvatore Garufi
  * @since 07/07/2025
- *
+ * inizializzazione delle seguenti variabili
  */
 public class Library {
     private LibraryItem[] items;
@@ -12,13 +12,23 @@ public class Library {
         itemCount = 0;
     }
 
+    /**
+     * creazione di un metodo pubblico usando i seguenti variabili
+     * @param book
+     * @param quantity
+     * il metodo presenta il seguente if "itemCount < items.length" nella quale incrementa il valore
+     * di  itemCount++ se è vero.
+     */
     public void addBook(IBook book, int quantity) {
         if (itemCount < items.length) {
             items[itemCount] = new LibraryItem(book, quantity);
             itemCount++;
         }
     }
-
+/**
+ * Creazione di un array in base al valore di itemCount
+ * che stamperà le frasi riportate in seguito
+ */
     public void printInventory() {
         for (int i = 0; i < itemCount; i++) {
             IBook book = items[i].getBook();
