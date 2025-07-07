ca193902e7bb0520d3472206ff3b7f617739c463
racket207-hub <rocco.lore@branchina.edu.it>
2025-07-07 12:42:32 +0200

    Commento di PrintedBook.java

--- PrintedBook.java
+++ PrintedBook.java
@@ -1,22 +1,48 @@
+/**
+ * @author Salvatore Garufi
+ * @since 07/07/2025ù
+ * si inizializza la classe PrintedBook implementando dalla classe IBook
+ * e le sue variabili stringa title, title;
+ * e la sua varabile int pages;
+ */
 public class PrintedBook implements IBook {
     private String title;
     private String author;
     private int pages;
 
+    /**
+     * si inizializza il metodo PrintedBook usando i seguenti parametri che abbiamo precedentemente inizzializzato
+     * e si usa il "this" per via dello stesso nome tra parametri e variabili
+     * @param title
+     * @param author
+     * @param pages
+     */
     public PrintedBook(String title, String author, int pages) {
         this.title = title;
         this.author = author;
         this.pages = pages;
     }
 
+    /**
+     * tramite il metodo getTitle
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * tramite il metodo getAuthor
+     * @return author
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * tramite il metodo getPages
+     * @return pages
+     */
     public int getPages() {
         return pages;
     }
